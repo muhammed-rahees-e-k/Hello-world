ca2b474c8ec66f9a4b4597c72a2b20d75d33c947
ek-rahees <rahees.ek.raihsoft@gmail.com>
2025-12-11 15:26:32 +0530

    changes

---
+++ src/components/Home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home: React.FC = () => {
+  return (
+    <div style={{ textAlign: "center", padding: "50px" }}>
+      <h1>Welcome to the Home Page</h1>
+      <p>This is the Home component.</p>
+    </div>
+  );
+};
+
+export default Home;
